a077c2adaa9e434e1db7bb6090f621a7aa594ffe
Ravi Nain <91778452+ravinainn@users.noreply.github.com>
2023-08-22 10:54:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,11 +17,23 @@ config({
 app.use(express.json());
 
 app.use(cookieParser());
-app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods: ["GET","POST","PUT","DELETE"],
-    credentials: true,
-}))
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URL],
+//     methods: ["GET","POST","PUT","DELETE"],
+//     credentials: true,
+// }))
+const allowedOrigins = ['http://localhost:5173']; // Add your frontend URL here
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Enable credentials (cookies, authorization headers)
+};
+app.use(cors(corsOptions));
 app.use(express.urlencoded({extended:true}))
 
 app.use("/api/v1/users" ,userRouter);
